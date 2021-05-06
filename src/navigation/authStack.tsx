1811f9acad334627313a4dc55b60be2c4dadbe04
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-05-06 17:08:13 +0100

    added the sign in stack

--- src/navigation/authStack.tsx
+++ src/navigation/authStack.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import SplashScreen from '../screens/getStarted/SplashScreen';
 import SignUp from '../screens/signUp/SignUp';
+import SignIn from '../screens/signIn/SignIn';
 const Stack = createStackNavigator();
 
 const AuthStack = () => {
@@ -11,6 +12,7 @@ const AuthStack = () => {
     }}>
       <Stack.Screen name="SplashScreen" component={SplashScreen} />
       <Stack.Screen name="SignUp" component={SignUp} />
+      <Stack.Screen name="SignIn" component={SignIn} />
     </Stack.Navigator>
   );
 };
