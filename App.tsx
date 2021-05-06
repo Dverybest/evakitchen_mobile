974068c02572e32a9f608467037bd374c5840508
Charles Best <charlesbest8@gmail.com>
2021-05-06 12:46:00 +0100

    add signup screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {
   SafeAreaView,
@@ -10,27 +10,33 @@ import {
   View,
 } from 'react-native';
 import SplashScreen from './src/screens/getStarted/SplashScreen';
-import { white } from './src/styles/colors';
+import {white} from './src/styles/colors';
 import AuthStack from './src/navigation/authStack';
 const Stack = createStackNavigator();
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
-
   return (
     <SafeAreaView style={styles.Container}>
-      <StatusBar backgroundColor={white} barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NavigationContainer fallback={<SplashScreen/>}>
-          <Stack.Screen name="Auth" component={AuthStack}/>
+      <StatusBar
+        backgroundColor={white}
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+      />
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Auth" component={AuthStack} />
+        </Stack.Navigator>
       </NavigationContainer>
-      <SplashScreen/>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   Container: {
-    flex:1
+    flex: 1,
   },
 });
 
---
+++ src/screens/signUp/SignUp.tsx
@@ -0,0 +1,16 @@
+import React, { StyleSheet, View } from 'react-native';
+
+const SignUp = ()=>{
+
+    return(
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container :{
+        flex:1
+    }
+})
+export default SignUp;
