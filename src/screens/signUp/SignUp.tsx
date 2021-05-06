258670e5409e32c0c18d495705a73849e0358dfb
Charles Best <charlesbest8@gmail.com>
2021-05-06 21:05:06 +0100

    complete Sign up component

--- src/screens/signUp/SignUp.tsx
+++ src/screens/signUp/SignUp.tsx
@@ -1,32 +1,60 @@
+import { useNavigation } from '@react-navigation/core';
 import React from 'react';
-import {StyleSheet,View,Text,Image} from 'react-native';
-import  logo from '../../assets/images/logo.png';
-import { white } from '../../styles/colors';
+import {StyleSheet, View, Text, Image,ScrollView, TouchableOpacity} from 'react-native';
+import logo from '../../assets/images/logo.png';
+import { ButtonPrimary, ButtonWhite } from '../../components/buttons';
+import {TextField} from '../../components/textfield';
+import {orange, white} from '../../styles/colors';
+import {TextStyle} from '../../styles/textStyle';
 
 const SignUp = () => {
-  return <View style={styles.container}>
-      <Image source={logo} style={styles.image}/>
-      <Text style={{
-          fontFamily:'Poppins-SemiBold',
-          fontSize:28,
-          lineHeight:42,
-      }}>
-      Create your account
+    const {navigate} = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.image} />
+      <Text style={TextStyle.semiBold}>Create your account</Text>
+      <ScrollView showsVerticalScrollIndicator={false}>
+      <Text style={TextStyle.medium}>
+        Please log in to continue from where you stopped
       </Text>
-  </View>;
+      <TextField placeholder="Fullname" containerStyle={{marginTop: 50}} />
+      <TextField keyboardType={'email-address'} placeholder="Email"  />
+      <TextField keyboardType={'phone-pad'} placeholder="Phone number" />
+      <TextField placeholder="Password" password={true} />
+      <ButtonPrimary
+            text="Sign In"
+            containerStyle={{marginBottom:31,marginTop:20}}
+            onPress={() => console.log('Hiii')}
+          />
+          <ButtonWhite
+            text="Sign In with Google"
+            containerStyle={{marginBottom:50}}
+          />
+          <TouchableOpacity
+            style={{alignItems: 'center'}}
+            onPress={() => navigate('SignIn')}>
+            <Text style={TextStyle.regular}>
+              {`Don’t have an account? `}
+              <Text style={[TextStyle.regular,{color: orange}]}>
+                Sign up
+              </Text>
+            </Text>
+          </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:white,
-    paddingHorizontal:25,
-    paddingTop:18
+    backgroundColor: white,
+    paddingHorizontal: 25,
+    paddingTop: 18,
   },
-  image:{
-      height:48,
-      width:48,
-      resizeMode:'contain'
-      
-  }
+  image: {
+    height: 48,
+    width: 48,
+    resizeMode: 'contain',
+  },
 });
 export default SignUp;
