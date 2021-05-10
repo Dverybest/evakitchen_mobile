bf1674e8252f2a5b83d194180633772011a3b42d
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-05-10 10:12:03 +0100

    set up the profile stack

---
+++ src/navigation/profileStack.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Profile from '../screens/profile/Profile';
+const Stack = createStackNavigator();
+
+const ProfileStack = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="Profile" component={Profile} />
+    </Stack.Navigator>
+  );
+};
+export default ProfileStack;
