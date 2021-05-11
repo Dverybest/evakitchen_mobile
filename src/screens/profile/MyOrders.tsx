33c877af96aaadccd91a89afd33b30e9e114ec18
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-05-11 02:20:20 +0100

    completed the design of the UI

--- src/screens/profile/MyOrders.tsx
+++ src/screens/profile/MyOrders.tsx
@@ -1,28 +1,80 @@
-import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
-import CardStyle from '../../styles/cardStyle';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {ScrollView, TouchableOpacity} from 'react-native-gesture-handler';
 import {grey, orange, white} from '../../styles/colors';
+import {TextStyle} from '../../styles/textStyle';
+import {Header} from '../../components/header';
+import OrderCard from './components/orderCard';
 import img2 from '../../assets/images/img2.jpg';
+import {string} from 'yup/lib/locale';
 
 const MyOrders = () => {
+  const [activeTab, setActiveTab] = useState<number>(1);
+  const orders = [
+    {
+      image: img2,
+      quantity: 2,
+      title: 'Egusi soup',
+      orderId: '#26234455',
+      arrival: 2,
+      status: 'Food on the way',
+    },
+    {
+      image: img2,
+      quantity: 2,
+      title: 'Egusi soup',
+      orderId: '#26234455',
+      arrival: 2,
+      status: 'Food on the way',
+    },
+  ];
   return (
     <View style={styles.container}>
+      <ScrollView></ScrollView>
+      <Header title="My orders" showGoBack={true} />
       <View style={styles.tabContainer}>
-        <TouchableOpacity style={styles.active}>
-          <Text>Active</Text>
+        <TouchableOpacity
+          style={activeTab === 1 ? styles.active : styles.inactive}
+          onPress={() => setActiveTab(1)}>
+          <Text
+            style={[
+              {...TextStyle.regular},
+              activeTab === 1 ? {color: white} : {color: orange},
+            ]}>
+            Active
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.inactive}>
-          <Text>History</Text>
+        <TouchableOpacity
+          style={activeTab === 2 ? styles.active : styles.inactive}
+          onPress={() => setActiveTab(2)}>
+          <Text
+            style={[
+              {...TextStyle.regular},
+              activeTab === 2 ? {color: white} : {color: orange},
+            ]}>
+            History
+          </Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.card}>
-        <View>
-          <Image source={img2} />
-        </View>
-        <View></View>
-        <View></View>
-      </View>
+      {/* <View style={{marginTop: 43}}> */}
+        <FlatList
+          data={orders}
+          keyExtractor={(item, index) => `orders${index}`}
+          renderItem={({item, index}) => (
+            <OrderCard
+              index={index}
+              image={item.image}
+              quantity={item.quantity}
+              title={item.title}
+              orderId={item.orderId}
+              arrival={item.arrival}
+              status={item.status}
+              primaryButtonTitle="Track order"
+              secondaryButtonTitle="Cancel"
+            />
+          )}
+        />
+      {/* </View> */}
     </View>
   );
 };
@@ -34,10 +86,11 @@ const styles = StyleSheet.create({
   },
   tabContainer: {
     marginTop: 39,
-    height: 50,
     alignSelf: 'center',
     flexDirection: 'row',
-    borderRadius: 25,
+    borderRadius: 150,
+    borderWidth: 1,
+    borderColor: grey,
   },
   active: {
     backgroundColor: orange,
@@ -46,25 +99,15 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     alignItems: 'center',
     justifyContent: 'center',
+    elevation: 10,
   },
   inactive: {
     height: 50,
-    width: 149.32,
+    width: 160.68,
     borderRadius: 25,
     alignItems: 'center',
     justifyContent: 'center',
-    borderWidth: 1,
     borderColor: grey,
-  },
-  card: {
-    ...CardStyle.card,
-    alignItems: 'center',
-    backgroundColor: white,
-    paddingHorizontal: 21,
-    paddingVertical: 16,
-    height: 213,
-    marginTop: 43,
-    flexDirection: 'row'
   },
 });
 export default MyOrders;
