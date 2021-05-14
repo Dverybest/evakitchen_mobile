6b6e421b083957a8548204137f59cccf78554a45
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-05-14 12:40:38 +0100

    added an onPresss type to the category interface

--- src/components/interface.tsx
+++ src/components/interface.tsx
@@ -3,6 +3,7 @@ import {ImageSourcePropType} from 'react-native';
 export interface ICategory {
   name: string;
   icon: ImageSourcePropType;
+  onPress?: () => void;
 }
 export interface IFood {
   title: string;
