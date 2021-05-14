f5388a7695a45b17d08845d452e6f66afce9cd4f
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-05-14 11:31:10 +0100

    added a flatlist to the component

--- src/screens/favourite/Favourites.tsx
+++ src/screens/favourite/Favourites.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import FoodCard from '../../components/foodCard';
 import SearchBar from '../../components/searchBar';
 import {white} from '../../styles/colors';
@@ -9,6 +9,24 @@ import img2 from '../../assets/images/img2.jpg';
 const Favourites = () => {
   const [searchText, setSearchText] = useState<string>('');
   const [favourite, setFavourite] = useState(false);
+  const favourites = [
+    {
+      image: img2,
+      rating: 4.6,
+      favourite,
+      setFavourite,
+      text: 'Egusi Soup with assorted meat',
+      amount: 1500,
+    },
+    {
+      image: img2,
+      rating: 4.5,
+      favourite,
+      setFavourite,
+      text: 'Egusi Soup with assorted meat',
+      amount: 1300,
+    },
+  ];
   return (
     <View style={styles.container}>
       <SearchBar
@@ -23,13 +41,19 @@ const Favourites = () => {
       <View style={{alignItems: 'center', marginTop: 19, marginBottom: 34}}>
         <Text style={{...TextStyle.semiBold}}>Favourites</Text>
       </View>
-      <FoodCard
-        image={img2}
-        rating={4.6}
-        favourite={favourite}
-        setFavourite={setFavourite}
-        text="Egusi Soup with assorted meat"
-        amount={1500}
+      <FlatList
+        data={favourites}
+        keyExtractor={(item, index) => `favourites${index}`}
+        renderItem={({item, index}) => (
+          <FoodCard
+            image={item.image}
+            rating={item.rating}
+            favourite={item.favourite}
+            setFavourite={item.setFavourite}
+            text={item.text}
+            amount={item.amount}
+          />
+        )}
       />
     </View>
   );
