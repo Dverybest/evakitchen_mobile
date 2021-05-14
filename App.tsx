b0fe7306e1afe7b5155413107d97fc53e04840df
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-05-14 11:41:50 +0100

    cleared up unecessary code

--- App.tsx
+++ App.tsx
@@ -2,26 +2,15 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  useColorScheme,
-} from 'react-native';
-import SplashScreen from './src/screens/getStarted/SplashScreen';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import {orange} from './src/styles/colors';
 import AuthStack from './src/navigation/authStack';
 import DashboardStack from './src/navigation/dashboard';
 const Stack = createStackNavigator();
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
     <SafeAreaView style={styles.Container}>
-      <StatusBar
-        backgroundColor={orange}
-        barStyle={'light-content' }
-      />
+      <StatusBar backgroundColor={orange} barStyle={'light-content'} />
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
