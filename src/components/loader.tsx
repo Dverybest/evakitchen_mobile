618c5883de58cf5d8efaf2ac601e3eabfcf0f3f3
Charles Best <charlesbest8@gmail.com>
2021-05-15 12:32:07 +0100

    add loader component

---
+++ src/components/loader.tsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import {Modal, View,StyleSheet} from 'react-native';
+import Spinner from 'react-native-spinkit';
+import { AppContext } from '../context/appContext';
+import { black500 } from '../styles/colors';
+
+const Loader = ()=>{
+    const {appState:{isLoading}} = useContext(AppContext)
+    return(
+        <Modal transparent={true} visible={isLoading}>
+            <View style={styles.container}>
+                <Spinner type={'Wave'} size={70} color="#fff" />
+            </View>
+        </Modal>
+    )
+}
+
+const styles =StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:black500
+    }
+})
+export default Loader;
