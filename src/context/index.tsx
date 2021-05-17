e3600e833184a1f833ab645fcce5933c4c6f307c
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-05-17 09:58:29 +0100

    added a check for data in the local storage

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,13 +1,27 @@
-import React, { } from 'react';
-import { IContextProvider } from '../interfaces/common';
+import React from 'react';
+import {IContextProvider} from '../interfaces/common';
+import SplashScreen from '../screens/getStarted/SplashScreen';
 import AppContextProvider from './appContext';
+import AuthContextProvider from './authContext';
+import {fetchFromStorage, StorageNames} from './storage';
 
-const Provider = ({children}:IContextProvider)=>{
-    return(
-        <AppContextProvider>
-            {children}
-        </AppContextProvider>
-    )
-}
+const Provider = ({children}: IContextProvider) => {
+  const [loading, setLoding] = React.useState(true);
+  const [initialValue, setInitialValue] = React.useState(null);
+  React.useEffect(() => {
+    fetchFromStorage(StorageNames.AUTH)
+      .then(value => setInitialValue(value))
+      .catch()
+      .finally(() => setLoding(false));
+  }, []);
+  if (loading) {
+    return <SplashScreen />;
+  }
+  return (
+    <AppContextProvider>
+      <AuthContextProvider value={initialValue}>{children}</AuthContextProvider>
+    </AppContextProvider>
+  );
+};
 
-export default Provider
+export default Provider;
