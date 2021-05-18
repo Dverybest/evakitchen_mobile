1f51cda03f062a08bfce2752ce6c2befddeca05a
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-05-18 16:13:59 +0100

    made some types optional

--- src/interfaces/cartContext.ts
+++ src/interfaces/cartContext.ts
@@ -1,15 +1,17 @@
 import {Dispatch} from 'react';
 import {IAction} from './common';
 
+export interface ICart {
+  quantity: number;
+  title: string;
+  favourite?: boolean;
+  amount: number;
+  description?: string;
+  rating?: number;
+}
+
 export interface ICartState {
-  items: {
-    quantity: number;
-    title: string;
-    favourite: boolean;
-    amount: number;
-    description: string;
-    rating: number;
-  }[] | null;
+  items: ICart[] 
 }
 export interface ICartContext {
   cartState: ICartState;
