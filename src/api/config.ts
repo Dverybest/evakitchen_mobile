b71f9966a602b8012f3e5d212069f1d1a5536954
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-05-26 10:02:30 +0100

    .

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,1 +1,2 @@
-export const baseUrl = "https://eva-resturant.herokuapp.com/api";
+export const baseUrl = 'https://eva-resturant.herokuapp.com/api';
+export const GOOGLE_MAP_API_KEY = 'AIzaSyDk- 7RBBvj3C9ilZl1UD9v1fMApZaDHj9M';
