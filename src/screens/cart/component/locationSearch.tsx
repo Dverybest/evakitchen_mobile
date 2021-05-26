8389dd4bc055ad1082a7f2ed9c76e8fc67d3700c
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-05-26 10:13:54 +0100

    integrated the map api with the UI

--- src/screens/cart/component/locationSearch.tsx
+++ src/screens/cart/component/locationSearch.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import {
   StyleSheet,
   Text,
@@ -11,6 +12,7 @@ import {TextStyle} from '../../../styles/textStyle';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import {FlatList, TextInput} from 'react-native-gesture-handler';
+import {GOOGLE_MAP_API_KEY} from '../../../api/config';
 
 interface ILocationSearch {
   text: string;
@@ -23,25 +25,33 @@ const LocationSearch = ({
   containerStyle,
   placeholder,
 }: ILocationSearch) => {
-  const places = [
-    {
-      name: 'Lifestyle and Golf City',
-      address: 'Km 7, Enugu-PH expressway, Enugu.',
-    },
-    {
-      name: 'Lifestyle and Golf City',
-      address: 'Km 7, Enugu-PH expressway, Enugu.',
-    },
-    {
-      name: 'Lifestyle and Golf City',
-      address: 'Km 7, Enugu-PH expressway, Enugu.',
-    },
-    {
-      name: 'Lifestyle and Golf City',
-      address: 'Km 7, Enugu-PH expressway, Enugu.',
-    },
-  ];
   const [searchText, setSearchText] = useState('');
+  const [showResults, setShowResults] = useState(false);
+  const [places, setPlaces] = useState([]);
+
+  const searchLocation = async (searchText: string) => {
+    const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${searchText}&region=NG&language=en&components=country:ng&types=''&key=${GOOGLE_MAP_API_KEY}`;
+    try {
+      const response = await axios({
+        url,
+        method: 'GET',
+      });
+      console.log(response);
+      setPlaces(response.data.results);
+      setShowResults(true);
+    } catch (error) {
+      setShowResults(false);
+      console.log(error);
+    }
+  };
+  const setLocation = (loaction: string) => {
+    setSearchText(loaction);
+    setShowResults(false);
+  };
+  const clearField = () => {
+    setSearchText('');
+    setShowResults(false);
+  };
   return (
     <View style={containerStyle}>
       <Text style={{...TextStyle.regular}}>{text}</Text>
@@ -51,7 +61,11 @@ const LocationSearch = ({
           placeholder={placeholder}
           placeholderTextColor={grey}
           value={searchText}
-          onChangeText={text => setSearchText(text)}
+          onChangeText={text => {
+            console.log(text);
+            setSearchText(text);
+            searchLocation(text);
+          }}
         />
         {searchText.length > 0 ? (
           <AntDesign
@@ -59,26 +73,36 @@ const LocationSearch = ({
             size={14}
             style={{alignSelf: 'center'}}
             color={black}
-            onPress={() => setSearchText('')}
+            onPress={clearField}
           />
         ) : null}
       </View>
-      {searchText.length > 0 ? (
-        <View style={{backgroundColor: white100, height: 175}}>
+      {showResults ? (
+        <View
+          style={{
+            backgroundColor: white100,
+            maxHeight: 175,
+            paddingBottom: 10,
+          }}>
           <FlatList
             data={places}
             keyExtractor={(_, index) => `items${index}`}
             renderItem={({item}) => (
-              <TouchableOpacity style={{flexDirection: 'row', marginTop: 29}}>
+              <TouchableOpacity
+                onPress={() => setLocation(item.name)}
+                style={{flexDirection: 'row', marginTop: 29}}>
                 <EvilIcons
                   name="location"
-                  style={{marginTop: 5, marginRight: 15}}
+                  style={{marginTop: 5}}
                   size={25}
                   color={black}
                 />
+                <View style={{width: 15}}></View>
                 <View>
                   <Text style={{...TextStyle.medium}}>{item.name}</Text>
-                  <Text style={{...TextStyle.regular}}>{item.address}</Text>
+                  <Text style={{...TextStyle.regular}}>
+                    {item.formatted_address}
+                  </Text>
                 </View>
               </TouchableOpacity>
             )}
