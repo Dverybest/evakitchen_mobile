9a4070673b5360646176992b4efe6564fc3c9edd
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-05-27 13:15:29 +0100

    added assets and types

--- src/interfaces/authContext.ts
+++ src/interfaces/authContext.ts
@@ -5,6 +5,7 @@ import { IUser } from "./user";
 export interface IAuthState {
   isAuthenticated: boolean;
   user: IUser | null;
+  token: string | null
 }
 export interface IAuthContext {
   authState: IAuthState;
