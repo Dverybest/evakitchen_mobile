3f0e4c67200b5c6db8d5374a0eb2757d1e3015bf
Iheanyi Ihekweaba <ihekweabai@gmail.com>
2021-06-11 11:08:36 +0100

    added the endpoint to be called

--- src/screens/profile/Profile.tsx
+++ src/screens/profile/Profile.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {grey100, orange, orange300, white} from '../../styles/colors';
 import {TextStyle} from '../../styles/textStyle';
@@ -8,14 +8,43 @@ import UploadOption from './components/modals/uploadOption';
 import {Header} from '../../components/header';
 import {TextField} from '../../components/textfield';
 import {ButtonPrimary} from '../../components/buttons';
+import {AuthContext} from '../../context/authContext';
+import {Formik} from 'formik';
+import {IUser} from '../../interfaces/user';
+import {userSchema} from './userSchema';
+import {useRequestProcessor} from '../../api/requestProcessor';
 
 const Profile = () => {
+  const {authState, dispatchAuthState} = useContext(AuthContext);
+  const {makeRequest} = useRequestProcessor();
   const [showUploadOption, setShowUploadOption] = useState(false);
   const [photoUrl, setPhotoUrl] = useState('');
   const handleUpload = (photo: any, setSelected: any) => {
     setPhotoUrl(photo.uri);
     setSelected({show: false});
     setShowUploadOption(false);
+  };
+
+  const handleSubmit = async (values: IUser) => {
+    const {response, error} = await makeRequest({
+      url: '/user/update',
+      method: 'PATCH',
+      payload: values,
+    });
+    if (error) {
+      console.log(error);
+    } else if (response && response?.success) {
+      console.log(response.data);
+      // const {authToken, ...userDetails} = response.data;
+      // dispatchAuthState({
+      //   type: ActionType.USER_DETAILS,
+      //   payload: userDetails,
+      // });
+      // dispatchAuthState({
+      //   type: ActionType.TOKEN,
+      //   payload: response.data.authToken,
+      // });
+    }
   };
 
   return (
@@ -61,26 +90,60 @@ const Profile = () => {
               </View>
             </View>
           </View>
-          <TextField
-            placeholder="Name"
-            containerStyle={{}}
-            value="Chisom Best"
-          />
-          <TextField
-            placeholder="Email"
-            containerStyle={{}}
-            value="c.best@gmail.com"
-          />
-          <TextField placeholder="eg. 07061011343" containerStyle={{}} />
-          <TextField
-            placeholder="eg. 07061011343"
-            containerStyle={{height: 85}}
-            multiline={true}
-          />
-          <ButtonPrimary
-            text="Save"
-            containerStyle={{marginBottom: 35, marginTop: 25}}
-          />
+          <Formik
+            initialValues={{
+              fullName: authState.user?.fullName || '',
+              email: authState.user?.email || '',
+              phoneNumber: authState.user?.phoneNumber || '',
+              address: authState.user?.address || '',
+            }}
+            validationSchema={userSchema}
+            onSubmit={values => handleSubmit(values)}>
+            {props => {
+              return (
+                <>
+                  <TextField
+                    placeholder="Name"
+                    containerStyle={{}}
+                    value={props.values.fullName}
+                    onChangeText={props.handleChange('fullName')}
+                    errorMessage={
+                      props.touched.fullName && props.errors.fullName
+                    }
+                  />
+                  <TextField
+                    placeholder="Email"
+                    containerStyle={{}}
+                    value={props.values.email}
+                    onChangeText={props.handleChange('email')}
+                    errorMessage={props.touched.email && props.errors.email}
+                  />
+                  <TextField
+                    placeholder="eg. 07061011343"
+                    containerStyle={{}}
+                    value={props.values.phoneNumber}
+                    onChangeText={props.handleChange('phoneNumber')}
+                    errorMessage={
+                      props.touched.phoneNumber && props.errors.phoneNumber
+                    }
+                  />
+                  <TextField
+                    placeholder="eg. 2a. Abakaliki Road"
+                    containerStyle={{height: 85}}
+                    multiline={true}
+                    value={props.values.address}
+                    onChangeText={props.handleChange('address')}
+                    errorMessage={props.touched.address && props.errors.address}
+                  />
+                  <ButtonPrimary
+                    text="Save"
+                    containerStyle={{marginBottom: 35, marginTop: 25}}
+                    onPress={props.handleSubmit}
+                  />
+                </>
+              );
+            }}
+          </Formik>
         </View>
       </ScrollView>
     </View>
