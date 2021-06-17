32787b546dd66adeb2bf45fbcf405e3875f67be9
Charles Best <charlesbest8@gmail.com>
2021-06-17 11:03:44 +0100

    delivery details api integration

--- src/screens/cart/DeliveryDetails.tsx
+++ src/screens/cart/DeliveryDetails.tsx
@@ -1,5 +1,5 @@
-import React, {useContext, useRef,useMemo} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useContext, useRef, useMemo, useState, useEffect} from 'react';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {Header} from '../../components/header';
 import {TextField} from '../../components/textfield';
 import {AuthContext} from '../../context/authContext';
@@ -9,76 +9,134 @@ import {v4 as uuidv4} from 'uuid';
 import 'react-native-get-random-values';
 import {ButtonFlutterWave} from '../../components/buttons';
 import LocationSearch from './component/locationSearch';
-import { ICart } from '../../interfaces/cartContext';
-import { CartContext } from '../../context/cartContext';
-
+import {ICart} from '../../interfaces/cartContext';
+import {CartContext} from '../../context/cartContext';
+import {useRequestProcessor} from '../../api/requestProcessor';
+import Success from '../../components/success';
 
 const DeliveryDetails = () => {
   const {authState} = useContext(AuthContext);
   const {cartState} = useContext(CartContext);
+  const {makeRequest} = useRequestProcessor();
+  const [showSuccess, setShowSuccess] = useState({show: false, message: ''});
+  const [details, setDetails] = useState({
+    fullName: '',
+    phoneNumber: '',
+    address: '',
+    deliveryType: 'instant Delivery',
+    shippingFee: 0,
+  });
+  const handleChange = (name: string, value: string) => {
+    if (name === 'address' && value != '') {
+      return setDetails(prev => ({...prev, [name]: value, shippingFee: 500}));
+    }
+    setDetails(prev => ({...prev, [name]: value}));
+  };
   const transactionReference = useRef(uuidv4());
- 
-  const deliveryFee = 100;
+
   const subTotal = useMemo(
     () =>
       cartState.items.reduce(
-        (amount: number, items: ICart) =>
-          Number(amount) + Number(items.amount) * Number(items.quantity),
-        0,
+        (amount: any, items: ICart) =>
+         [ Number(amount[0]) + Number(items.amount) * Number(items.quantity),Number(amount[1]) + Number(items.discount)],
+        [0,0],
       ),
     [cartState.items],
   );
-  const total = subTotal + deliveryFee;
+  const orderItems = useMemo(
+    () =>
+      cartState.items.reduce(
+        (arr: any, item: ICart) => [
+          ...arr,
+          {menu: item._id, quantity: item.quantity},
+        ],
+        [],
+      ),
+    [cartState.items],
+  );
+  const handleRedirect = async (data: any) => {
+    console.log({data},{...details, transactionId: data.transaction_id, orderItems});
+    const {response, error} = await makeRequest({
+      method: 'post',
+      url: `/makeorder`,
+      payload: {...details,discount:subTotal[1], transactionId: data.transaction_id, orderItems},
+    });
+    if (error) {
+      console.log(error)
+    } else if (response) {
+      setShowSuccess({show: true, message: response.message});
+    }
+  };
   return (
     <View style={styles.container}>
       <Header title={'Delivery detail'} />
-      <View style={{flex: 1, marginHorizontal: 25}}>
-        <Text style={{...TextStyle.medium, width: 230,fontSize:16}}>
-          Please enter your delivery details
-        </Text>
-        <Text style={{...TextStyle.regular, marginTop: 30}}>
-          Who will receive this package?
-        </Text>
+      <Text style={{...TextStyle.regular, fontSize: 18, marginHorizontal: 25}}>
+        Please enter delivery details
+      </Text>
+      <ScrollView style={{flex: 1, marginHorizontal: 25}}>
+      {/* <Success
+            visible={showSuccess.show}
+            // onPress={() => setShowSuccess({show: false, message: ''})}
+            text={showSuccess.message}
+          /> */}
         <View>
-          <View style={{flexDirection: 'row'}}>
+          <View style={{flexDirection: 'row', marginTop: 15}}>
             <TextField
               keyboardType="default"
+              onChangeText={text => handleChange('fullName', text)}
               placeholder="Name"
+              value={details.fullName}
               mainContainerStyle={{flex: 1}}
             />
             <View style={{width: 10}}></View>
             <TextField
+              onChangeText={text => handleChange('phoneNumber', text)}
               keyboardType="number-pad"
-              placeholder="Phone Number"
+              value={details.phoneNumber}
               mainContainerStyle={{flex: 1}}
+              placeholder="Phone Number"
             />
           </View>
-          <LocationSearch text="Delivery location" containerStyle={{}} />
+          <LocationSearch
+            handleChange={value => handleChange('address', value)}
+            placeholder="Delivery location"
+            containerStyle={{}}
+          />
         </View>
-       <View style={{justifyContent:'flex-end',flex:1}}>
-       <View style={styles.priceDetails}>
-              <Text style={[TextStyle.medium]}>Sub Total</Text>
-              <Text style={[TextStyle.medium]}>{`₦${subTotal}`}</Text>
-            </View>
-            <View style={styles.priceDetails}>
-              <Text style={[TextStyle.medium]}>Delivery fee</Text>
-              <Text style={[TextStyle.medium]}>{`₦${deliveryFee}`}</Text>
-            </View>
-            <View style={styles.priceDetails}>
-              <Text style={[TextStyle.medium]}>Total</Text>
-              <Text style={[TextStyle.medium]}>{`₦${total}`}</Text>
-            </View>
-        <ButtonFlutterWave
-          transactionReference={transactionReference.current ?? ''}
-          email={authState.user?.email}
-          amount={total}
-          containerStyle={{
-            marginVertical: 20,
-          }}
-          text="Continue to make payment"
-        />
-      </View>
-       </View>
+        <View style={{justifyContent: 'flex-end', flex: 1, marginTop: 50}}>
+          <View style={styles.priceDetails}>
+            <Text style={[TextStyle.medium]}>Sub Total</Text>
+            <Text style={[TextStyle.medium]}>{`₦${subTotal}`}</Text>
+          </View>
+          <View style={styles.priceDetails}>
+            <Text style={[TextStyle.medium]}>Delivery fee</Text>
+            <Text style={[TextStyle.medium]}>{`₦${details.shippingFee}`}</Text>
+          </View>
+          <View style={styles.priceDetails}>
+            <Text style={[TextStyle.medium]}>Total</Text>
+            <Text style={[TextStyle.medium]}>{`₦${
+              subTotal[0] + details.shippingFee
+            }`}</Text>
+          </View>
+          <ButtonFlutterWave
+            disabled={
+              !(
+                details.fullName &&
+                details.phoneNumber.length > 10 &&
+                details.address
+              )
+            }
+            transactionReference={transactionReference.current ?? ''}
+            email={authState.user?.email ?? ''}
+            handleRedirect={handleRedirect}
+            amount={subTotal[0] + details.shippingFee}
+            containerStyle={{
+              marginVertical: 20,
+            }}
+            text="Continue to make payment"
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
